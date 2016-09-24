38ecf0a0736ece24bdb7cd3a963384cf79e8f414
Alexander <shiale14@student.westerdals.no>
2016-09-24 16:11:16 +0200

    make home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,16 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+    var movementStrength = 100;
+    var height = movementStrength / $(window).height();
+    var width = movementStrength / $(window).width();
+    $("#top-image").mousemove(function(e){
+        var pageX = e.pageX - ($(window).width() / 2);
+        var pageY = e.pageY - ($(window).height() / 2);
+        var newvalueX = width * pageX * -1 - 25;
+        var newvalueY = height * pageY * -1 - 50;
+        $('#top-image').css("background-position", newvalueX+"px     "+newvalueY+"px");
+    });
+});
