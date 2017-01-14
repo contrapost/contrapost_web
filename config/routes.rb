882b5af163a782f30321bf34c695337cd1a70a53
Alexander <shiale14@student.westerdals.no>
2017-01-14 19:02:00 +0100

    add random front page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  root "pages\##{Random.rand(1...3)}"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   Rails.application.routes.draw do
